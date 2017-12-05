c53c54bd9514b802abec422f924dfc50638fd648
302 <302@choongang>
2017-12-05 15:51:04 +0900

    variable

---
+++ firstProject/HelloWorld.java
@@ -0,0 +1,19 @@
+package firstProject;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("삼다수");
+		// sysout
+		System.out.println("이것이 자바다");
+		/* 
+		 * 여러줄의 주석
+		 * 은 이렇게 작성합니다.
+		 */
+		if (args.length >= 2) {
+			System.out.println(args[0]);
+			System.out.println(args[1]);
+		}
+	}
+}
+
+
---
+++ variables/Variable.java
@@ -0,0 +1,41 @@
+package variables;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// 기본 데이터 타입
+		// 정수형
+		int i = 22000000;
+		System.out.println(i);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		long l = 999999999222L;
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		short s = 3300;
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		byte b = 1;
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		
+		// 실수형
+		float f = 22.2F;
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE);
+		double d = 62.2;
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.MIN_VALUE);
+		
+		// boolean
+		boolean bool = true; //true 또는 false
+		
+		// char
+		char c = '가';
+		System.out.println(c);
+
+	}
+}
+
+
+
