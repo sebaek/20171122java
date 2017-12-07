cd2545f3ec80427a9eb415a739fcd88ee80805d3
302 <302@choongang>
2017-12-07 11:31:04 +0900

    exception

--- exception/MyMain2.java
+++ exception/MyMain2.java
@@ -1,30 +1,36 @@
 package exception;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class MyMain2 {
 	public static void main(String[] args) {
 		File file = new File("src/text.txt");
 		FileReader fr = null;
-		fr = new FileReader(file);
-		System.out.println((char) fr.read());
-		fr.close();
-//		try {
-//			fr = new FileReader(file);
-//			System.out.println((char) fr.read());
-//
-//		} catch (FileNotFoundException e) {
-//			System.out.println("파일을 찾지 못했습니다.");
-//		} catch (IOException e) {
-//			System.out.println("입출력 exception");
-//		} finally {
-//			try {
-//				fr.close();
-//			} catch (IOException e) {
-//				
-//			}
-//		}
+//		fr = new FileReader(file);
+//		System.out.println((char) fr.read());
+//		fr.close();
+		try {
+			fr = new FileReader(file);
+			System.out.println((char) fr.read());
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("파일을 찾지 못했습니다.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("입출력 exception");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fr.close();
+			} catch (IOException e) {
+				
+			}
+		}
 		
 		
 		
